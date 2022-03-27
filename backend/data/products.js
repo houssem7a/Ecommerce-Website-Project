740712c5f098f8ad3353e14abdc3f160f84c7577
houssem <houssemsaid55@gmail.com>
2022-03-27 16:09:14 +0200

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: "STRANGER SPITFIRE",
+    image: "/images/stranger-spitfire.jpg",
+    description:
+      "Le Bmx Complet Stranger Spitfire 20.75 Gloss Bleu navy est un super milieu de gamme avec une géométrie réactive et moderne. Equipé au top avec du périphérique Primo: pneus, pédales, selle combo, pédales et guidon 4 pièces de 9 pouces.",
+    brand: "BMX",
+    category: "Mechanical",
+    price: 629.99,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "Dblocks Big Ripper 29",
+    image: "/images/dblocks.jpg",
+    description:
+      "Le Se Bike DBlocks Big Flyer 29 2021 Camouflage est le vélo signature de Darnell Meyers aKa Dblocks, voici la version 2021!",
+    brand: "SE Bikes",
+    category: "Mechanical",
+    price: 829,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "OM FLYER 26’ GOLD",
+    image: "/images/om-flyer-26-gold.jpg",
+    description:
+      "le Se Bikes OM Flyer 26' Gold 2021 c'est la rolls des gros bmx au look vintage! Idéal pour les pilotes à partir de 1.65m.",
+    brand: "SE Bikes",
+    category: "Mechanical",
+    price: 799,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "GT BIKES 29“ POWER PERFORMER GREY",
+    image: "/images/gt-bikes.jpg",
+    description:
+      "le GT Power Performer 29“ vous emmènera là où vous désirez grâce au moteur Bafang de 250W !  Le Power Performer est monté avec des pièces de qualité approuvées par GT Bicycles.",
+    brand: "GT BIKES",
+    category: "Electronics",
+    price: 2699.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "BMX FLATLAND KHE 19'",
+    image: "/images/flatland-khe-19.jpg",
+    description:
+      "Le BMX Flatland Chris Böhm est équipé de du rotor BMX Affix 360° breveté, de freins en U-Brakes avant & arrière  et d'une potence courte .",
+    brand: "BMX",
+    category: "Mechanical",
+    price: 499.99,
+    countInStock: 7,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: "RADIO BIKE REVO PRO FS 20“ ",
+    image: "/images/bike-revo-pro.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "RADIO",
+    category: "Mechanical",
+    price: 399.99,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+];
+
+export default products;
